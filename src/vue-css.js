d08ac530706d99d671f9f640d3fe9c1e4f751312
jrc03c <jrc03c@pm.me>
2022-10-04 18:39:10 -0500

    ...

---
+++ src/vue-css.js
@@ -0,0 +1,45 @@
+const VueComponentCSSPlugin = {
+  install(app) {
+    const oldComponentFunction = app.component
+
+    app.component = function (name, component) {
+      const oldMountedFunction = component.mounted
+      const oldUnmountedFunction = component.unmounted
+      const id = Math.random().toString().split(".")[1]
+
+      component.mounted = function () {
+        const self = this
+        const styleElement = document.createElement("style")
+        styleElement.innerHTML = self.css
+        styleElement.id = id
+        document.head.appendChild(styleElement)
+        if (oldMountedFunction) oldMountedFunction()
+      }
+
+      component.unmounted = function () {
+        const styleElement = document.getElementById(id)
+
+        if (styleElement) {
+          document.head.removeChild(styleElement)
+        }
+
+        if (oldUnmountedFunction) oldUnmountedFunction()
+      }
+
+      if (component.components) {
+        Object.keys(component.components).forEach(key => {
+          const nestedComponent = component.components[key]
+          app.component(key, nestedComponent)
+        })
+      }
+
+      oldComponentFunction(name, component)
+    }
+  },
+}
+
+export default VueComponentCSSPlugin
+
+if (typeof window !== "undefined") {
+  window.VueComponentCSSPlugin = VueComponentCSSPlugin
+}
--- vue-css.js
+++
@@ -1,7 +0,0 @@
-export const VueComponentCSSPlugin = {
-  install() {},
-}
-
-if (typeof window !== "undefined") {
-  window.VueComponentCSSPlugin = VueComponentCSSPlugin
-}
